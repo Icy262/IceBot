ee15b424dcdcc6b15797a081bbd4e4f03fb3ca05
Icy26 <Icy26@>
2026-04-21 22:03:34 -0400

    Make the fields in the action structs crate public

--- src/actions.rs
+++ src/actions.rs
@@ -1,31 +1,35 @@
-use std::fs::DirBuilder;
+use crate::block::{Block, Direction};
 
 //TODO: implement support for online servers
 pub (crate) struct Join {
-	address: String, //ip or srv record
-	username: String, //player username
+	pub(crate) address: String, //ip or srv record
+	pub(crate) username: String, //player username
 }
 
 pub (crate) struct Walk {
-	block: Block, //block that feet will occupy
+	pub(crate) block: Block, //block that feet will occupy
+}
+
+pub (crate) struct Look {
+	pub(crate) block: Block, //block that bot will look at
 }
 
 pub (crate) struct Jump {
 }
 
 pub (crate) struct BreakBlock {
-	position: Block, //block to break
-	face: Direction, //face of block we are hitting
+	pub(crate) position: Block, //block to break
+	pub(crate) face: Direction, //face of block we are hitting
 }
 
 //For if we just want a block in a particular position
 pub (crate) struct PlaceBlock {
-	position: Block, //position of the block
-	rotation: Direction, //direction of the placed block
+	pub(crate) position: Block, //position of the block
+	pub(crate) rotation: Direction, //direction of the placed block
 }
 
 //For if we want to specifiy the block we are placing against
 pub (crate) struct PlaceBlockAgainst {
-	position: Block, //position of the block we are placing against
-	rotation: Direction, //direction of the placed block
+	pub(crate) position: Block, //position of the block we are placing against
+	pub(crate) rotation: Direction, //direction of the placed block
 }
