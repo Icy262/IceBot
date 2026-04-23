b37875e8ac3004b22c3fcb54744ec3d99be81749
Icy26 <Icy26@>
2026-04-23 10:42:47 -0400

    Fix type error

--- src/entity.rs
+++ src/entity.rs
@@ -1,7 +1,9 @@
+use crate::data_types::MCMetadata;
+
 pub(crate) struct Entity {
 	pub(crate) block_type: String,
 	pub(crate) metadata: MCMetadata,
-	pub(crate) x: double,
-	pub(crate) y: double,
-	pub(crate) z:double,
+	pub(crate) x: f64,
+	pub(crate) y: f64,
+	pub(crate) z:f64,
 }
