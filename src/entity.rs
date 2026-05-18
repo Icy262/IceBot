f7e97bd650ad34b62da8f0f05af443f12bbb14be
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-05-18 19:22:00 -0400

    Create EntityPositionandLook struct to hold data about an entities position and where it is facing

--- src/entity.rs
+++ src/entity.rs
@@ -1,9 +1,16 @@
 use crate::data_types::MCMetadata;
 
-pub(crate) struct Entity {
-	pub(crate) block_type: String,
-	pub(crate) metadata: MCMetadata,
+pub(crate) struct EntityPositionAndLook {
 	pub(crate) x: f64,
 	pub(crate) y: f64,
 	pub(crate) z: f64,
+	pub(crate) yaw: f64,
+	pub(crate) pitch: f64,
+	pub(crate) on_ground: bool,
+}
+
+pub(crate) struct Entity {
+	//TODO: implement
+	// pub(crate) block_type: String,
+	// pub(crate) metadata: MCMetadata,
 }
