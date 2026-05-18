4d76c3a79801d61a2cfc067ad247a55322ea1684
Icy26 <Icy26@>
2026-05-18 19:22:42 -0400

    Split movements from actions and create a do action function for each to convert that action into packets and send them

--- src/actions.rs
+++ src/actions.rs
@@ -1,19 +1,24 @@
+use std::net::TcpStream;
+
 use crate::block::{Block, Coordinates, Direction};
+use crate::action_translator;
+use crate::packets::write_packet;
+
+pub(crate) fn do_action(action: action_translator::Actions, server_connection: &mut TcpStream) {
+	let packets = action_translator::to_packets(action);
+	for packet in packets {
+		write_packet(server_connection, packet);
+	}
+}
 
 //TODO: implement support for online servers
 pub(crate) struct Join {
 	pub(crate) username: String, //player username
 }
 
-pub(crate) struct Walk {
-	pub(crate) position: Coordinates, //Coordinates that the feet will occupy
-}
-
 pub(crate) struct Look {
 	pub(crate) target: Coordinates, //Coordinates that bot will look at
 }
-
-pub(crate) struct Jump {}
 
 pub(crate) struct BreakBlock {
 	pub(crate) position: Coordinates, //coordinates of the block to break
@@ -26,7 +31,7 @@ pub(crate) struct PlaceBlock {
 	pub(crate) rotation: Direction, //direction of the placed block
 }
 
-//For if we want to specifiy the block we are placing against
+//For if we want to specify the block we are placing against
 pub(crate) struct PlaceBlockAgainst {
 	pub(crate) position: Coordinates, //position of the block we are placing against
 	pub(crate) rotation: Direction, //direction of the placed block
