2e97f05fa35077dc1eea24eab814e890055672c6
Icy26 <Icy26@>
2026-05-29 19:19:21 -0400

    Create a struct to contain the movement and actions that should be performed by a bot in a single tick. Used to allow a future task to actions + movements generator to generate paired instructions. This will prevent the movements and actions from desyncing.

---
+++ src/behaviour.rs
@@ -0,0 +1,7 @@
+use crate::{actions::Actions, movements::Movements};
+
+//Represents the movement and action of a bot in a single tick
+pub(crate) struct Behaviour {
+	pub(crate) movement: Movements,
+	pub(crate) action: Actions, 
+}
