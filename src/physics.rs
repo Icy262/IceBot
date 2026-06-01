1b6a8be20ef24a29ef1b60c4af78b56d02f6e384
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-05-31 22:01:52 -0400

    Split the physics processing from PLAYER state updating

--- src/physics.rs
+++ src/physics.rs
@@ -1,56 +1,66 @@
-use crate::{block::{self, Block, Coordinates}, bot::PLAYER, world::{WORLD_MODEL, World}};
+use crate::{block::{self, Block, Coordinates}, bot::PLAYER, world::{WORLD_MODEL, World}, player::Player};
+use crate::movements::Movements;
+use crate::packets::Packets;
+
 
 //Not a perfect implementation, but good enough for now
-//Does one tick of motion. Horizontal acceleration is handled elsewhere
-pub(crate) fn process_motion() {
-	PLAYER.with_borrow_mut(|player| {
-		let last_tick_y = player.y;
-
+//Calculates one tick of motion. Horizontal acceleration is handled elsewhere
+pub(crate) fn process_motion() -> Player {
+	PLAYER.with_borrow(|old_player| {
+		let mut new_player = (*old_player).clone();
+		
 		//From the minecraft wiki: https://minecraft.wiki/w/Entity#Motion
 		//Update position
-		player.x += player.vx;
-		player.y += player.vy;
-		player.z += player.vz;
+		new_player.x += new_player.vx;
+		new_player.y += new_player.vy;
+		new_player.z += new_player.vz;
 
 		//Update acceleration
-		player.vy -= 0.08;
+		new_player.vy -= 0.08;
 
 		//Update drag
-		player.vx *= 0.91;
-		player.vy *= 0.98;
-		player.vz *= 0.91;
+		new_player.vx *= 0.91;
+		new_player.vy *= 0.98;
+		new_player.vz *= 0.91;
 
 		//TODO: implement fall damage
 		//TODO: implement handling for parital hitboxes like slabs
 		//check if player colliding with ground (block it is entering is not air)
-		if player.on_ground {
+		if new_player.on_ground {
 			//check if should start falling
-			let block_below = World::get_block(crate::block::Coordinates { x: player.x as i32, y: player.y as i32 - 1, z: player.z as i32 });
+			let block_below = World::get_block(crate::block::Coordinates { x: new_player.x as i32, y: new_player.y as i32 - 1, z: new_player.z as i32 });
 			if block_below.is_some() {
 				//TODO: Implement support for falling through things that are not air but are solid (eg. grass or water)
 				if block_below.expect("Should not be None because we checked it is some").block_type == "air" {
-					player.on_ground = false;
+					new_player.on_ground = false;
+				} else {
+					//if on ground, stop vertical velocity and unclip the old_player from the block below
+					new_player.vy = 0.0;
+					new_player.y = new_player.y.ceil()
 				}
 			}
 		} else {
 			//check if player should stop falling
-			if player.y < last_tick_y.floor() && last_tick_y > last_tick_y.floor() {
-				let block_below = World::get_block(crate::block::Coordinates { x: player.x as i32, y: player.y as i32 - 1, z: player.z as i32 });
+			if new_player.y < old_player.y.floor() && old_player.y > old_player.y.floor() {
+				let block_below = World::get_block(crate::block::Coordinates { x: new_player.x as i32, y: new_player.y as i32 - 1, z: new_player.z as i32 });
 				if block_below.is_some() {
 					//TODO: Implement support for falling through things that are not air but are solid (eg. grass or water)
 					if block_below.expect("Should not be None because we checked it is some").block_type != "air" {
-						player.on_ground = true;
+						new_player.on_ground = true;
 					}
 				}
 			}
 		}
 
-		//if on ground, stop vertical velocity and unclip the player from the block
-		if player.on_ground {
-			player.vy = 0.0;
-			player.y = player.y.ceil()
-		}
-
 		//TODO: implement hitboxes on other axes
+
+		return new_player;
 	})
+}
+
+//Calls process_motion to calculate the motion and then actually updates the PLAYER
+pub(crate) fn update_position() {
+	PLAYER.with_borrow_mut(|player| {
+		*player = process_motion();
+	});
 }
