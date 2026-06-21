be09ebfdcabad3fe380883e61a2ca26ed13e4856
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-06-20 20:30:56 -0400

    Implement cost function for price from s -> s_prime

--- src/pathfind.rs
+++ src/pathfind.rs
@@ -85,4 +85,83 @@ impl Path {
 			self.compute_shortest_path();
 		}
 	}
+
+	//cost of moving from s to s_prime where s_prime is succ(s)
+	fn c(s: &Coordinates, s_prime: &Coordinates) -> u32{
+		//get the blocks at eye, foot, and below the player to see if breaking or placing is necessary
+		let position_head = Coordinates {
+			y: s_prime.y + 1,
+			..*s_prime
+		};
+		let position_feet = Coordinates { ..*s_prime };
+		let position_support = Coordinates {
+			y: s_prime.y - 1,
+			..*s_prime
+		};
+
+		let block_head = World::get_block(position_head);
+		let block_feet = World::get_block(position_feet);
+		let block_support = World::get_block(position_support);
+
+		//very rough approximation of the costs
+		//TODO: improve the cost prediction algorithm
+		//head is free if it doesn't need to be removed, if not, it is the cost to remove
+		let head_price = match block_head {
+			Some(block_head) => {
+				match (*BLOCK_REGISTRY)
+					.get(&block_head.block_id)
+					.expect("ID of block should be in BLOCK_REGISTRY")
+					.collision
+				{
+					Collision::NonSolid => 1,
+					Collision::Liquid => 3,
+					Collision::Solid => 5,
+				}
+			}
+			None => {
+				//We don't know what the block is, so optimistically assume it is free
+				0
+			}
+		};
+
+		//feet are free if it doesn't need to be removed, if not, the cost is the cost to remove
+		let feet_price = match block_feet {
+			Some(block_feet) => {
+				match (*BLOCK_REGISTRY)
+					.get(&block_feet.block_id)
+					.expect("ID of block should be in BLOCK_REGISTRY")
+					.collision
+				{
+					Collision::NonSolid => 1,
+					Collision::Liquid => 3,
+					Collision::Solid => 5,
+				}
+			}
+			None => {
+				//We don't know what the block is, so optimistically assume it is free
+				0
+			}
+		};
+
+		//support block price is free if solid, if not, cost is cost to place block
+		let support_price = match block_support {
+			Some(block_support) => {
+				match (*BLOCK_REGISTRY)
+					.get(&block_support.block_id)
+					.expect("ID of block should be in BLOCK_REGISTRY")
+					.collision
+				{
+					Collision::NonSolid => 5,
+					Collision::Liquid => 5,
+					Collision::Solid => 0,
+				}
+			}
+			None => {
+				//We don't know what the block is, so optimistically assume it is free
+				0
+			}
+		};
+
+		return head_price + feet_price + support_price;
+	}
 }
