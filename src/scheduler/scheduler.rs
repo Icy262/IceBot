f74a7810cee803d200a3954d10d4d4aa3748576d
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-06-26 20:46:43 -0400

    Change from vecdeque to vec because deque isn't necessary

--- src/scheduler/scheduler.rs
+++ src/scheduler/scheduler.rs
@@ -3,13 +3,17 @@ use std::collections::VecDeque;
 use crate::task::Tasks;
 
 pub(crate) struct Schedule {
-	tasks: VecDeque<Tasks>,
+	tasks: Vec<Tasks>,
 }
 
 impl Schedule {
 	pub(crate) fn new() -> Self {
 		Self {
-			tasks: VecDeque::new(),
+			tasks: Vec::new(),
 		}
+	}
+
+	pub(crate) fn push_task(&mut self, new_task: Tasks) {
+		self.tasks.push(value);
 	}
 }
