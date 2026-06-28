d2ba10d383e9e02706333c51ba63e172a0aaa579
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-06-28 17:02:14 -0400

    Make get_next_behaviour return a behaviour that does nothing so the code will compile. Will impl properly later

--- src/tasks/go_to.rs
+++ src/tasks/go_to.rs
@@ -1,5 +1,7 @@
 use crate::behaviour::behaviour::Behaviour;
 use crate::world::block::Coordinates;
+use crate::behaviour::movements::{Movements, NoInput};
+use crate::behaviour::actions::{Actions, DoNothing};
 
 //Pathfind to a position and generate the required actions to get there
 pub(crate) struct GoTo {
@@ -8,6 +10,7 @@ pub(crate) struct GoTo {
 
 impl GoTo {
 	pub(crate) fn get_next_behaviour(&self) -> Behaviour {
+		return Behaviour { movement: Movements::NoInput(NoInput {}), action: Actions::DoNothing(DoNothing {})};
 	}
 
 	pub(crate) fn complete(self) -> bool {
