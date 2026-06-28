a1323d5b7150c3fb6d0cc1b872b519ae7a0f39bf
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-06-28 17:02:10 -0400

    Make get_next_behaviour return a behaviour that does nothing so the code will compile. Will impl properly later

--- src/tasks/gather.rs
+++ src/tasks/gather.rs
@@ -1,4 +1,6 @@
 use crate::behaviour::behaviour::Behaviour;
+use crate::behaviour::movements::{Movements, NoInput};
+use crate::behaviour::actions::{Actions, DoNothing};
 
 //Gather a specified amount of an item. Can do this by mining or collecting from storage containers
 pub(crate) struct Gather {
@@ -8,6 +10,7 @@ pub(crate) struct Gather {
 
 impl Gather {
 	pub(crate) fn get_next_behaviour(&self) -> Behaviour {
+		return Behaviour { movement: Movements::NoInput(NoInput {}), action: Actions::DoNothing(DoNothing {})};
 	}
 	
 	pub(crate) fn complete(self) -> bool {
