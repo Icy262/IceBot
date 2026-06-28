d872665c792d9bc5ef218764deea2666f6076170
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-06-28 17:22:44 -0400

    Impl new

--- src/tasks/go_to.rs
+++ src/tasks/go_to.rs
@@ -1,14 +1,21 @@
 use crate::behaviour::actions::{Actions, DoNothing};
 use crate::behaviour::behaviour::Behaviour;
 use crate::behaviour::movements::{Movements, NoInput};
+use crate::pathfinding::pathfind::Path;
 use crate::world::block::Coordinates;
 
 //Pathfind to a position and generate the required actions to get there
 pub(crate) struct GoTo {
-	pub(crate) position: Coordinates,
+	path: Path,
 }
 
 impl GoTo {
+	pub(crate) fn new(start: &Coordinates, goal: &Coordinates) -> Self {
+		return Self {
+			path: Path::initialize(start, goal),
+		}
+	}
+
 	pub(crate) fn get_next_behaviour(&self) -> Behaviour {
 		return Behaviour {
 			movement: Movements::NoInput(NoInput {}),
@@ -19,4 +26,5 @@ impl GoTo {
 	pub(crate) fn complete(self) -> bool {
 		return false;
 	}
+
 }
