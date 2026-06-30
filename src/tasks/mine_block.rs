ee49b45544ba727ee3856c55c72d64f6cb0ce799
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-06-30 17:09:39 -0400

    Implement new function

--- src/tasks/mine_block.rs
+++ src/tasks/mine_block.rs
@@ -1,10 +1,18 @@
-use std::path::Path;
-
-use crate::world::block::Coordinates;
+use crate::{pathfinding::pathfind::Path, world::block::Coordinates};
 
 //Pathfind to next to a block, break it, and (optionally) pick up the item it drops (if it drops one)
-pub(crate) struct mine_block {
+pub(crate) struct MineBlock {
 	position: Coordinates,
 	path: Path,
 	pickup_item: bool,
+}
+
+impl MineBlock {
+	pub(crate) fn new(position: &Coordinates, pickup_item: bool) -> Self {
+		return Self {
+			position: *position,
+			path: Path::new(position),
+			pickup_item: pickup_item,
+		};
+	}
 }
