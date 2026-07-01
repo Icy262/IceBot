a1a3622015bc9f50a74da93baad162d8e2b59012
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-06-30 21:22:16 -0400

    Revise new impl to get start from current PLAYER pos

--- src/tasks/go_to.rs
+++ src/tasks/go_to.rs
@@ -1,6 +1,7 @@
 use crate::behaviour::actions::{Actions, DoNothing};
 use crate::behaviour::behaviour::Behaviour;
 use crate::behaviour::movements::{Movements, NoInput};
+use crate::bot::PLAYER;
 use crate::pathfinding::pathfind::Path;
 use crate::world::block::Coordinates;
 
@@ -10,10 +11,18 @@ pub(crate) struct GoTo {
 }
 
 impl GoTo {
-	pub(crate) fn new(start: &Coordinates, goal: &Coordinates) -> Self {
+	pub(crate) fn new(goal: &Coordinates) -> Self {
+		let start = PLAYER.with_borrow(|player| {
+			return Coordinates {
+				x: player.x.floor() as i32,
+				y: player.y.floor() as i32,
+				z: player.z.floor() as i32,
+			}
+		});
+		
 		return Self {
 			path: Path::initialize(start, goal),
-		}
+		};
 	}
 
 	pub(crate) fn get_next_behaviour(&self) -> Behaviour {
