57a27581da699d4a20b39a728523d97547eac4f2
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-06-30 22:25:26 -0400

    Implement rought mockup of get_next_behaviour

--- src/hierarchical_task_network/hierarchical_task_network.rs
+++ src/hierarchical_task_network/hierarchical_task_network.rs
@@ -1,5 +1,28 @@
+use crate::behaviour::actions::{Actions, DoNothing};
+use crate::behaviour::behaviour::Behaviour;
+use crate::behaviour::movements::NoInput;
 use crate::tasks::tasks::Tasks;
+use crate::behaviour::movements::Movements;
 
 pub(crate) struct HierarchicalTaskNetwork {
 	tasks: Vec<Tasks>,
+}
+
+impl HierarchicalTaskNetwork {
+	pub(crate) fn get_next_behaviour(&mut self) -> Behaviour {
+		let Some(task) = self.tasks.get_mut(0) else {
+			return Behaviour {
+				movement: Movements::NoInput(NoInput {}),
+				action: Actions::DoNothing(DoNothing {}),
+			};
+		};
+
+		//if elementary task, return it, if not, break it down, and call this fn recursively
+		return match task {
+			Tasks::GoTo(task) => task.get_next_behaviour(),
+			Tasks::Gather(task) => task.get_next_behaviour(),
+			Tasks::FindItem(task) => task.get_next_behaviour(),
+			Tasks::ClearRegion(task) => task.get_next_behaviour(),
+		};
+	}
 }
