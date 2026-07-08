0d00e55e296b398b64915efe2b0513849a2a43f5
Icy26 <114495528+Icy262@users.noreply.github.com>
2026-07-08 19:05:56 -0400

    Split position component in entity position and look into seperate struct for use in pathfinding too

--- src/world/entity.rs
+++ src/world/entity.rs
@@ -1,9 +1,13 @@
 use crate::network::data_types::MCMetadata;
 
-pub(crate) struct EntityPositionAndLook {
+pub(crate) struct Position {
 	pub(crate) x: f64,
 	pub(crate) y: f64,
 	pub(crate) z: f64,
+}
+
+pub(crate) struct EntityPositionAndLook {
+	pub(crate) position: Position,
 	pub(crate) yaw: f64,
 	pub(crate) pitch: f64,
 	pub(crate) on_ground: bool,
